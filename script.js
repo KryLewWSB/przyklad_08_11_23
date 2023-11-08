7ef693628c7061028004fc69d548dd236fa5d66e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-08 13:00:34 +0100

    7

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var wyslij = document.getElementById("wyslij");
-
-wyslij.addEventListener("click", function (e){
-    e.preventDefault();
-    var imie = document.getElementById("imie").value;
-    console.log(imie);
-});
+const numbers = [5, 3, 8, 45, 74, 2];
+console.log(Math.max(numbers));
+console.log(Math.max(...numbers));
