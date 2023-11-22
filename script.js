8a97de2fbe42c96ee78f0909db585a46143a219e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-22 10:12:03 +0100

    8lottozad3

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const numbers = [5, 3, 8, 45, 74, 2];
-console.log(Math.max(numbers));
-console.log(Math.max(...numbers));
+const liczbyGracza = new Set();
+const liczbyWylosowane = new Set();
+
+while(liczbyGracza.size < 6){
+    let liczbaPobrana = parseInt(prompt(`Podaj liczbę ${liczbyGracza.size+1}:`));
+    if (liczbaPobrana > 1 && liczbaPobrana < 49 ){
+        liczbyGracza.add( liczbaPobrana );
+    }
+}
+
+while(liczbyWylosowane.size < 6){
+    liczbyWylosowane.add( Math.round(Math.random()*48+1) );
+}
+
+//liczbyWylosowane.has() sprawdzanie
+
